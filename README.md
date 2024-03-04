80fdcd0ebcc2fbc02403c5fbb51a3247de2c9d61
Hernan J. Larrea <hernan@larrea.com.ar>
2024-03-04 12:49:04 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,57 @@
+# Introduction
+
+This repository contains a simple game built with PyGame Zero for one of my kids. The my objectives behind this project are, beyond building something for him and to get him interested in Computers, to:
+- Learn about the dynamics of writing games in Python
+- Learn how to leverage WebAssembly (WASM) to distribute Python projects over the web
+
+# Requirements
+
+This game has been built using pyenv and virtual environments for isolating the dependencies required for executing it. Check the local `.python-version` file, if not using pyenv to ensure your local environment is compatible with the project's set up.
+
+# How to start the project
+
+There are different alternatives on how to run this project.
+
+## Local run
+
+Run the following command standing in the project's directory:
+
+```
+pgzrun main.py
+```
+
+## Local WASM execution
+
+Run the following command standing in the project's directory:
+
+```
+pygbag .
+```
+
+Then navigate to `https://127.0.0.1:8000`.
+
+## Docker image
+
+Run the following command standing in the project's directory:
+
+```
+docker build -t valosrun:latest .
+docker run --rm -d -p 8000:80 valosrun:latest
+```
+
+Then navigate to `https://127.0.0.1:8000`.
+
+# How to play
+
+TODO
+
+# Infrastructure
+
+TODO
+
+# Roadmap
+
+- Fix the loading issues in browsers: the WASM component might not load always, depends on the browser (Chrome, Firefox or Safari) and the platform (PC or Mobile). Might need documentation instead of a proper technical issue to be solved.
+- Fix performance issues: depending on the browser and platform where the WASM component is executed, the game might be accelerated rendering it unplayable.
+- Users are able to input their name while playing on PC, but not on mobile as the keyboard won't pop up in the main screen. Might require to render a keyboard on the screen.
+- Implement a leaderboard. Upon `gameMode == 2` condition (game over), save the combination of user and score in an external source. Then build a static website to render the results.
---
+++ main.py
@@ -0,0 +1,171 @@
+import asyncio,pgzero,pgzrun,pygame
+from random import randint
+from string import ascii_letters
+
+def draw():
+    screen.clear()
+    screen.blit("background",(0,0))
+    if gameMode == 0:
+        screen.blit("welcome",(120,80))
+        screen.draw.text(f"Tu nombre: {user}", color="white", topleft=(200, 280))
+        playAgain.draw()
+    if gameMode == 1:
+        valo.draw()
+        monster.draw()
+        screen.draw.text(f"Puntos: {score}", color="white", topleft=(10, 10))
+    elif gameMode == 2:
+        screen.blit("gameover",(160,120))
+        screen.draw.text(f"{score} puntos", color="white", center=(320, 280),fontsize=50)
+        playAgain.draw()
+
+def update():
+    global monster
+    global score
+    global speed
+    global jumpDuration
+    global monsterStartPoint
+    global gameMode
+   
+    if gameMode == 0:
+        # Welcome screen
+        placePlayAgain()
+    elif gameMode == 1:
+        # Game dynamics
+        # Monster moves
+        monster.x = monster.x - speed
+        if valo.colliderect(monster):
+            # Check for loss condition
+            gameMode = 2
+            saveScore()
+        elif monster.x < 0:
+            # Send in a new monster
+            monster = selectMonster()
+            placeMonster()
+            score = score + 100
+            setDifficulty()
+    elif gameMode == 2:
+        # Game over screen
+        placePlayAgain()
+
+def on_mouse_down(pos):
+    if gameMode == 0:
+        # Start the game
+        if playAgain.collidepoint(pos):
+            resetGame()
+    if gameMode == 1:
+        if(valo.y == 350):
+            animate(valo, pos=(60,150), tween="decelerate", duration=jumpDuration, on_finished=returnPosition)
+    elif gameMode == 2:
+        # Restart the game
+        if playAgain.collidepoint(pos):
+            resetGame()
+
+def returnPosition():
+    animate(valo, pos=(60,350),tween="accelerate", duration=jumpDuration)
+
+def on_key_down(key):
+    # User inputing their name
+    # TODO: not compatible with mobile browsers
+    global user
+    global gameMode
+    if gameMode == 0:
+        if key == 8:
+            user = user[0:-1]
+        elif key == 13:
+            gameMode = 1
+        elif (key >= 65 and key <= 90) or (key >= 97 and key <= 122):
+            pressedKey = chr(key)
+            user += pressedKey
+    
+def selectMonster():
+    monsterSelected = randint(1,5)
+    if monsterSelected == 1:
+        return Actor("chica")
+    elif monsterSelected == 2:
+        return Actor("bunny")
+    elif monsterSelected == 3:
+        return Actor("foxy")
+    elif monsterSelected == 4:
+        return Actor("freddy2")
+    elif monsterSelected == 5:
+        return Actor("freddy")
+
+def placeValo():
+    valo.x = 60
+    valo.y = 350
+
+def placeMonster():
+    monster.x = 700
+    monster.y = 350
+
+def placePlayAgain():
+    playAgain.x = 320
+    playAgain.y = 380
+
+def saveScore():
+    pass
+
+def setDifficulty():
+    global speed
+    global jumpDuration
+     # Set difficulty
+    if score == 1000:
+        speed = 7
+        jumpDuration = jumpDuration - 0.1
+    elif score == 2000:
+        speed = 9
+        jumpDuration = jumpDuration - 0.1
+    elif score == 3000:
+        speed = 11
+        jumpDuration = jumpDuration - 0.1
+    elif score == 4000:
+        speed = 13
+    elif score == 5000:
+        speed = 14
+    elif score == 6000:
+        speed = 15
+
+def resetGame():
+    global gameMode
+    global score
+    global speed
+    global jumpDuration
+    gameMode = 1
+    score = 0
+    speed = 5
+    jumpDuration = 0.7
+    placeMonster()
+
+async def main():
+    placeValo()
+    placeMonster()
+    pgzrun.go()
+    await asyncio.sleep(0)
+
+#
+# Constants
+#
+
+WIDTH = 640
+HEIGHT = 480
+monsterStartPoint = 700
+background = pygame.image.load("images/background.jpg")
+
+#
+# Variables
+#
+
+user = ""
+score = 0
+speed = 5
+jumpDuration = 0.7
+gameMode = 0
+valo = Actor("valo")
+monster = selectMonster()
+playAgain = Actor("play")
+
+#
+# Game start
+#
+
+asyncio.run(main())
